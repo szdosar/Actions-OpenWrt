a610649baf45db396340329f4a6191e7da35e074
szdosar <dosar@live.com>
2023-11-17 16:33:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,6 @@
 
 # Add a feed source
 # sed -i -E 's/#(src-git.+)(helloworld.+)/\1\2/' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# sed -i '$a src-git openappfilter https://github.com/szdosar/OpenAppFilter' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# sed -i '$a src-git aliyundrivewebdav https://github.com/messense/aliyundrive-webdav.git' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
+sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld/tree/main' feeds.conf.default
