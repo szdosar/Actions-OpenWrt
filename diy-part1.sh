ae02008af8fc20b51ec5a96b52efacffbfd316df
szdosar <dosar@live.com>
2024-01-30 11:10:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,11 @@
 
 # Add a feed source
 # sed -i -E 's/#(src-git.+)(helloworld.+)/\1\2/' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
+# sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;main' feeds.conf.default
+# sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
 sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;main' feeds.conf.default
-sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
+sed -i '$a src-git openwrtpackages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
