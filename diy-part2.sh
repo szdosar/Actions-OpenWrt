fc5128fc2618a874fe3c38712bcbcff28924fd82
szdosar <dosar@live.com>
2024-02-14 10:59:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/time_starttransfer/time_total/g' feeds/passwall2/luci-app-passwall2/luasrc/controller/passwall2.lua
-sed -i 's/option filter_aaaa 1/option filter_aaaa 0/g' package/network/services/dnsmasq/files/dhcp.conf
-# Enable firewall4
-# sed -i 's/+firewall/+uci-firewall/g' feeds/luci/applications/luci-app-firewall/Makefile
-# sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
