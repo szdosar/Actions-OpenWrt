4bbb35b22788e2cb43e640080d8927fe6e2e00d3
szdosar <dosar@live.com>
2024-09-08 14:39:29 +0800

    Update diy-part1_small.sh

--- diy-part1_small.sh
+++ diy-part1_small.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+bash <(curl -Ls https://raw.githubusercontent.com/szdosar/ish/master/1key-go-amd.sh)
